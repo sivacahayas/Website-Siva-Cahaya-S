0722b16d99d77c63158a34bb035fa50521e204f8
sivacahayas <sivacahayasuhailahh@gmail.com>
2025-03-19 20:33:09 +0700

    javascript.js

--- script.js
+++ script.js
@@ -20,4 +20,19 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     setInterval(createStar, 300);
+
+    const toggleButton = document.getElementById("mode-toggle");
+    toggleButton.addEventListener("click", function () {
+        document.body.classList.toggle("dark-mode");
+
+        if (document.body.classList.contains("dark-mode")) {
+            localStorage.setItem("theme", "dark");
+        } else {
+            localStorage.setItem("theme", "light");
+        }
+    });
+
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark-mode");
+    }
 });
